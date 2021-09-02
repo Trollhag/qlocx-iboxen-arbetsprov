f4261d86764b1e864ddb7491cfc2867333603d7c
Oscar Trollhag <oscar.trollhag@gmail.com>
2021-09-02 15:16:01 +0200

    Fixed locker and compartment selection when creating package

--- api/models/package.js
+++ api/models/package.js
@@ -9,15 +9,14 @@ module.exports = class extends Package {
   static async create({ transporter, address, coordinates, dimensions }) {
     dimensions.sort()
     const lockers = await Locker.aggregate([
-      { $unwind: '$compartments' },
+      { $lookup: { from: 'compartments', localField: 'compartments', foreignField: '_id', as: 'compartments' } },
       { $match: { 'compartments.status': Compartment.statusList.EMPTY } },
-      { $group: { _id: '$_id', compartments: { $push: "$compartments" } } }
     ])
     lockers.sort((a,b) => Math.abs(coordinates[0] - a.coordinates[0] + coordinates[1] - a.coordinates[1]) - Math.abs(coordinates[0] - b.coordinates[0] + coordinates[1] - b.coordinates[1]))
-    console.log(lockers);
-    let l = 0, compartment;
+    let locker, l = 0, compartment;
     while (compartment === undefined) {
-      const compartments = lockers[l].compartments.filter(({ status }) => status === Compartment.statusList.EMPTY)
+      locker = lockers[l]
+      const compartments = locker.compartments.filter(({ status }) => status === Compartment.statusList.EMPTY)
         .sort((a,b) => a.dimensions[0] * a.dimensions[1] * a.dimensions[2] - b.dimensions[0] * b.dimensions[1] * b.dimensions[2])
       const c = compartments.find((comp) => dimensions[0] < comp.dimensions[0] && dimensions[1] < comp.dimensions[1] && dimensions[2] < comp.dimensions[2])
       if (c) {
@@ -28,7 +27,8 @@ module.exports = class extends Package {
         throw new Error('No locker found.')
       }
     }
-    await compartment.overwrite({ status: Compartment.statusList.OCCUPIED }).save()
+    console.log(locker.coordinates)
+    await Compartment.updateOne({ _id: compartment._id }, { status: Compartment.statusList.OCCUPIED })
     return await super.create({ transporter, address, coordinates, dimensions, compartment: compartment._id })
   }
 }
